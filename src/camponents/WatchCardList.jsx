bea177ed0b34e4b42c1217ef12075727eb72a54a
nik afz <nikafz980@gmail.com>
2024-01-20 13:18:04 +0400

    feat: add select form control

--- src/camponents/WatchCardList.jsx
+++ src/camponents/WatchCardList.jsx
@@ -1,27 +1,55 @@
 import React from "react";
 import WatchCard from "./WatchCard";
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 
 function CardList() {
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   return(
 
     <div className="Card-list">
-      <div>
-        <select>
-          <option value="">sorted by</option>
-          <option value="p">price</option>
-          <option value="p">price</option>
-          <option value="p">price</option>
-          <option value="p">price</option>
-          <option value="p">price</option>
-        </select>
-      </div>
+      {/* <div>
+
+        <FormControl color="secondary" sx={{ m: 0, minWidth: 200 }}>
+          <InputLabel id="demo-simple-select-helper-label">Order by</InputLabel>
+          <Select
+            labelId="demo-simple-select-helper-label"
+            value={age}
+            label="handle"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={1}>prise</MenuItem>
+            <MenuItem value={2}>raiting</MenuItem>
+          </Select>
+          <ButtonGroup fullWidth color="secondary" variant="outlined" >
+            <Button>111</Button>
+            <Button>111</Button>
+            <Button>111</Button>
+          </ButtonGroup>
+        </FormControl>
+
+      </div> */}
       <div className="Card-list--container">
         <WatchCard/>
         <WatchCard/>
         <WatchCard/>
         <WatchCard/>
         <WatchCard/>
-        <WatchCard/>0
+        <WatchCard/>
         <WatchCard/>
         <WatchCard/>
         <WatchCard/>
