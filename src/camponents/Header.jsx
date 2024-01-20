ea65bde31a31be85fe0b4efd5fec968071058c5f
nik afz <nikafz980@gmail.com>
2024-01-20 13:13:43 +0400

    fix: icon badge

--- src/camponents/Header.jsx
+++ src/camponents/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: 0,
+    top: 28,
+    border: `2px solid ${theme.palette}`,
+    padding: '0 4px',
+  },
+}));
 
 function Header(){
   return(
@@ -10,10 +21,16 @@ function Header(){
         <h1>LuxeTick</h1>
         <div className="header--buttons">
           <a href="">
-            <ShoppingBagOutlinedIcon sx={{ fontSize: 30 }}/>
+            
+            <Badge badgeContent={1} color="secondary" >
+              <ShoppingBagOutlinedIcon sx={{ fontSize: 30 }}/>
+            </Badge>
+            
           </a>
           <a href="">
-            <FavoriteBorderOutlinedIcon sx={{ fontSize: 30 }}/>
+            <Badge badgeContent={0} color="secondary" >
+              <FavoriteBorderOutlinedIcon sx={{ fontSize: 30 }}/>
+            </Badge>
           </a>
 
         </div>
