5b58de150692b436eda04a05c4b1456e2be897a4
nik afz <nikafz980@gmail.com>
2024-01-21 23:55:08 +0400

    feat: buttons for side pages

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,24 @@
+import { Button } from "@mui/material";
 import CardList from "../camponents/WatchCardList";
 import WavyHeader from "../camponents/WavyHeader";
-
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import ShoppingCartCheckoutRoundedIcon from '@mui/icons-material/ShoppingCartCheckoutRounded';
 
 function Cart() {
   return (
     <>
       <WavyHeader name="Cart"/>
       <div className="spacer2"></div>
+      <div className="side-page-button--container">
+        <Button className="r-b" sx={{"&:hover": {borderColor: "rgb(135, 99, 227)" , backgroundColor: "rgba(215, 199, 255, 0.557)", borderWidth:"2px"}, borderRadius:"10px", color:"#8763e3", borderColor:"rgb(135, 99, 227)"}} 
+        size="large" variant="outlined" >
+          remove all
+        </Button>
+        <Button sx={{borderRadius:"10px", backgroundColor:"#8763e3", "&:hover": {backgroundColor:"#6a4eb1"}}}
+        variant="contained" disableElevation size="large" endIcon={<ShoppingCartCheckoutRoundedIcon/>}>
+          purchase
+        </Button>
+      </div>
       <CardList/>
       
     </>
--- src/pages/Fav.jsx
+++ src/pages/Fav.jsx
@@ -1,11 +1,18 @@
+import { Button } from "@mui/material";
 import CardList from "../camponents/WatchCardList";
 import WavyHeader from "../camponents/WavyHeader";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Fav() {
   return (
     <>
       <WavyHeader name="Favorits"/>
       <div className="spacer2"></div>
+      <div className="side-page-button--container">
+        <Button className="r-b" endIcon={<DeleteIcon/>} sx={{"&:hover": {borderColor: "rgb(135, 99, 227)" , backgroundColor: "rgba(215, 199, 255, 0.557)", borderWidth:"2px"}, borderRadius:"10px", color:"#8763e3", borderColor:"rgb(135, 99, 227)"}} size="large" variant="outlined">
+          remove all
+        </Button>
+      </div>
       <CardList/>
       
     </>
