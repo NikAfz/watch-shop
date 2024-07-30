ed32d7998ce47aac5d2e8dcc74f9d7e8023f5da8
nik afz <nikafz980@gmail.com>
2024-07-30 19:32:48 +0330

    fix : fav page

--- src/pages/Fav.jsx
+++ src/pages/Fav.jsx
@@ -2,23 +2,53 @@ import { Button } from "@mui/material";
 import CardList from "../camponents/WatchCardList";
 import WavyHeader from "../camponents/WavyHeader";
 import DeleteIcon from '@mui/icons-material/Delete';
+import NoItem from "../camponents/NoItem";
 
 function Fav(props) {
 
   const fixData = props.data.filter(watch => watch.fav === true)
+
+  const removeFav = () => {
+    const newData = props.data.map((item)=>{
+      return { ...item, fav: false }
+    });
+    localStorage.setItem("dataKey", JSON.stringify(newData))
+    props.setData(newData);
+  }
+
   return (
     <>
-      <WavyHeader name="Favorits"/>
+      <WavyHeader name="Favorits" />
       <div className="spacer2 spacer2-1"></div>
-      <div className="side-page-button--container">
-        <Button className="r-b" endIcon={<DeleteIcon/>} sx={{"&:hover": {borderColor: "rgb(135, 99, 227)" , backgroundColor: "rgba(215, 199, 255, 0.557)", borderWidth:"2px"}, borderRadius:"10px", color:"#8763e3", borderColor:"rgb(135, 99, 227)"}} size="large" variant="outlined">
-          remove all
-        </Button>
-      </div>
-      <CardList data={fixData} setData={props.setData}/>
-      
+      {fixData.length >= 1 ? (
+        <>
+          <div className="side-page-button--container">
+            <Button
+              className="r-b"
+              endIcon={<DeleteIcon />}
+              sx={{
+                "&:hover": {
+                  borderColor: "rgb(135, 99, 227)",
+                  backgroundColor: "rgba(215, 199, 255, 0.557)",
+                  borderWidth: "2px",
+                },
+                borderRadius: "10px",
+                color: "#8763e3",
+                borderColor: "rgb(135, 99, 227)",
+              }}
+              size="large"
+              variant="outlined"
+              onClick={removeFav}
+            >
+              remove all
+            </Button>
+          </div>
+          <CardList data={fixData} setData={props.setData} />
+        </>
+      ) : <NoItem title="Favorited"/>}
     </>
-  )
+  );
+  
 }
 
 export default Fav;
